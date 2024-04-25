45895b550311249ebe599c4d413e408f493fe40a
Lyubomira Bileva <lubka.bileva@gmail.com>
2024-04-26 00:13:17 +0300

    angular-w1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world-2201681034';
+  title = 'Lyubi Bubi';
+
+  toggleSection(event: MouseEvent) {
+    const sectionContent = (event.currentTarget as HTMLElement).nextElementSibling;
+    if (sectionContent) {
+      const allSections = document.querySelectorAll('.section-content');
+      for (let i = 0; i < allSections.length; i++) {
+        const section = allSections[i] as HTMLElement;
+        if (section !== sectionContent && section.classList.contains('show')) {
+          section.classList.remove('show');
+        }
+      }
+      sectionContent.classList.toggle('show');
+    }
+  }
 }
